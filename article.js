3af51a5c46c868802bef96595738e24be88ad183
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-08-20 11:32:54 +0200

    init

---
+++ article.js
@@ -0,0 +1,12 @@
+var article = document.querySelector('[itemprop=articleBody]') ||
+	document.querySelector('.article-body');
+
+if (article) {
+	chrome.runtime.sendMessage('showPageAction');
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request === 'stripArticle' && sender) {
+		document.body.innerHTML = article.innerHTML;
+	}
+});
---
+++ bg.js
@@ -0,0 +1,11 @@
+var currentTab;
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request === 'showPageAction' && sender) {
+		chrome.pageAction.show(sender.tab.id);
+	}
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+	chrome.tabs.sendMessage(tab.id, 'stripArticle');
+});
