d8d83c889928b3b2aa7d10050f393d978b9689d6
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-04-20 15:21:52 +0200

    clear styles when stripping

--- article.js
+++ article.js
@@ -4,6 +4,19 @@
 	var article = document.querySelector('[itemprop=articleBody]') ||
 		document.querySelector('.article-body');
 
+	var clearStyles = function() {
+		document.querySelectorAll('style,link[rel="stylesheet"]').forEach(function(e) {
+			e.remove();
+		});
+
+		var attrs = ['style', 'bgcolor', 'color', 'width', 'height', 'face', 'size', 'align', 'border'];
+		document.querySelectorAll('*').forEach(function(e) {
+			attrs.forEach(function(attr) {
+				e.removeAttribute(attr);
+			});
+		});
+	};
+
 	if (article) {
 		chrome.runtime.sendMessage('showPageAction');
 	}
@@ -11,6 +24,7 @@
 	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		if (request === 'stripArticle' && sender) {
 			document.body.innerHTML = article.innerHTML;
+			clearStyles();
 		}
 	});
 })();
